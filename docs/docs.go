0e19a6b2d42e32ea5d418f375d4bd4397d06349b
Yeneineh Seiba <yeneinehseiba@gmail.com>
2025-04-28 05:54:24 -0700

    refine the host url in the swagger doc

--- docs/docs.go
+++ docs/docs.go
@@ -5478,7 +5478,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://yene-hub-ls0y.onrender.com",
+	Host:             "yene-hub-ls0y.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Hub API",
